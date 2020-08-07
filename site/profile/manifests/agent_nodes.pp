9a42323ec570d17f08bd8e71a8a9d86e70ec7935
SuperGordon52 <69326666+SuperGordon52@users.noreply.github.com>
2020-08-07 16:22:33 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes  {
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm.us-central1-a.c.axiomatic-sonar-285617.internal':}
+  dockeragent::node {'db.puppet.vm.us-central1-a.c.axiomatic-sonar-285617.internal':}
 }
