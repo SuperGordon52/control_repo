4c36d578a53c546b4570a2930e67cc0e46d4fb97
SuperGordon52 <69326666+SuperGordon52@users.noreply.github.com>
2020-08-07 15:14:02 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,13 +9,13 @@ node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => "Welcome to Master",
+    content => “Welcome to ${fqdn}”,
     owner => 'root',
   }
 }
-node 'web.puppet.vm' { 
+node /^web/ { 
   include role::app_server
 }
-node 'db.puppet.vm' {
+node /^db/ {
   include role::db_server
 }
